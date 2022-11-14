c9d799174b003871dc22cee18cbc76433e3dd4e5
Pranita Pradhan <Pranitapradhan150@gmail.com>
2022-11-14 23:14:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+  let currDate = new Date();
+currDate = currDate.getHours();
+let greet = "";
+const cssStyle = {};
+if(currDate >= 1 && currDate < 12){
+  greet = "enjoy your morning with a cup of hot tea.Have a nice day";
+  cssStyle.color = "green";
+}else if(currDate >= 12 && currDate < 20){
+  greet = "woohops..the day time is very hot";
+  cssStyle.color = "yellow";
+} else{
+  greet = "such a peaceful night.have a sweet dream";
+  cssStyle.color = "blue";
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Hello Manoj <span style = {cssStyle}> {greet} </span> ...</h1>
     </div>
   );
 }
